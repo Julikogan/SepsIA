642e67320306cb51b87671f4377bcd65fa03e156
Julikogan <48243253@est.ort.edu.ar>
2025-06-12 14:27:45 -0300

    commit

--- src/controllers/resultadosController.js
+++ src/controllers/resultadosController.js
@@ -2,9 +2,11 @@ import { obtenerResultadoPorSignoVital } from '../services/resultadosService.js'
 
 export const getResultadoPorSignoVital = async (req, res) => {
   const { id } = req.params;
+  console.log("ID recibido:", id); 
 
   try {
     const resultado = await obtenerResultadoPorSignoVital(id);
+    console.log("Resultado encontrado:", resultado);
 
     if (!resultado) {
       return res.status(404).json({ message: 'Resultado no encontrado para este signo vital' });
--- src/services/resultadosService.js
+++ src/services/resultadosService.js
@@ -3,7 +3,7 @@ import prisma from '../models/prisma.js';
 export const obtenerResultadoPorSignoVital = async (idSignoVital) => {
   return await prisma.resultado.findUnique({
     where: {
-      idSignosVitales: Number(idSignoVital)
+      id_signo_vital: Number(idSignoVital)
     }
   });
 };
