c355b8597de121f46696a2ca5194020a9c4358fe
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2025-06-13 14:07:24 -0300

    fix en vercel.json y cliente de prisma

--- src/services/resultadosService.js
+++ src/services/resultadosService.js
@@ -1,4 +1,5 @@
-import prisma from '../models/prisma.js';
+import { PrismaClient } from '../../prisma/client.js';
+const prisma = new PrismaClient();
 
 export const obtenerResultadoPorSignoVital = async (idSignoVital) => {
   return await prisma.resultado.findUnique({
